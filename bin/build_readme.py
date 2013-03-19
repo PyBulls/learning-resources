2466bfcd1d7222aee24c64676082588679acaa03
Jason Keene <jasonkeene@gmail.com>
2013-03-18 21:41:30 -0400

    make it to where you can run the build script anywhere

--- bin/build_readme.py
+++ bin/build_readme.py
@@ -7,19 +7,21 @@ import yaml
 
 
 README_TEMPLATE = 'readme_template.md'
+SCRIPT_DIR = os.path.dirname(__file__)
+DATA_DIR = os.path.join(SCRIPT_DIR, '../data')
 
 
 def get_context():
-    yaml_files = [fname for fname in os.listdir('data') if fname.endswith('.yaml')]
+    yaml_files = [fn for fn in os.listdir(DATA_DIR) if fn.endswith('.yaml')]
     context = {}
     for yaml_file in yaml_files:
-        with open(os.path.join('data', yaml_file)) as f:
+        with open(os.path.join(DATA_DIR, yaml_file)) as f:
             context[yaml_file[:-5]] = yaml.load(f)
     return context
 
 
 def main(template_file=README_TEMPLATE):
-    with open(os.path.join(os.path.dirname(__file__), template_file)) as f:
+    with open(os.path.join(SCRIPT_DIR, template_file)) as f:
         template = Template(f.read())
     print template.render(**get_context())
 
